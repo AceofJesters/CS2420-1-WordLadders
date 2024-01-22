1634460745a9ed0ac644f96f7d19a253323fac16
Basil Nelson <ems.nelson@hotmail.com>
2024-01-22 08:27:14 -0700

    sketch LadderGame.java

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -15,27 +15,36 @@ public class LadderGame {
     public ArrayList<String> oneAway(String word, boolean withRemoval) {
         ArrayList<String> words = new ArrayList<>();
 
-        // TODO: Write some good stuff here
+        while (!withRemoval) {
+            for (String wordTwo : this.dictionary.get(word.length())) {
+                int wordDifference = diff(word, wordTwo);
+
+                if (wordDifference == word.length() - 1) {
+                    words.add(wordTwo);
+                }
+            }
+            withRemoval = true;
+        }
 
         return words;
     }
 
     public void listWords(int length, int howMany) {
-        // TODO: Write some good stuff here
+        for (int i = 0; i < howMany; i++) {
+            System.out.println(this.dictionary.get(length).get(i));
+        }
     }
 
-    /*
-        Reads a list of words from a file, putting all words of the same length into the same array.
-     */
+        //Reads a list of words from a file, putting all words of the same length into the same array.
     private void readDictionary(String dictionaryFile) {
         File file = new File(dictionaryFile);
         ArrayList<String> allWords = new ArrayList<>();
 
-        //
+
         // Track the longest word, because that tells us how big to make the array.
         int longestWord = 0;
         try (Scanner input = new Scanner(file)) {
-            //
+
             // Start by reading all the words into memory.
             while (input.hasNextLine()) {
                 String word = input.nextLine().toLowerCase();
@@ -43,8 +52,12 @@ public class LadderGame {
                 longestWord = Math.max(longestWord, word.length());
             }
 
-            // TODO: You need to do something here to organize the words into groups/arrays of words with the same size
-
+            for(int i = 0; this.dictionary.size() <= longestWord; i++) {
+                this.dictionary.add(new ArrayList<String>());
+            }
+            for (String word : allWords) {
+                this.dictionary.get(word.length()).add(word);
+            }
         }
         catch (java.io.IOException ex) {
             System.out.println("An error occurred trying to read the dictionary: " + ex);
