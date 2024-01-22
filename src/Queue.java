df0a35555c073e45173b355de10d0a6df78f2af7
Basil Nelson <ems.nelson@hotmail.com>
2024-01-22 07:33:05 -0700

    set up LadderNode class

--- src/Queue.java
+++ src/Queue.java
@@ -1,5 +1,32 @@
-public class Queue {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Queue<E> {
+
+    public void enqueue(E value){
+
     }
+
+    public E dequeue(){
+
+        return null;
+    }
+
+    public boolean isEmpty(){
+
+        return false;
+    }
+
+    public class LadderNode<E> {
+        public E nodeValue;
+        public LadderNode<E> next;
+
+        public LadderNode() { }
+        public LadderNode(E nodeValue) {
+            this.nodeValue = nodeValue;
+        }
+    }
+    //Use a linked list for the internal queue data representation.  You must write this linked list code yourself,
+    // may not use a Java collection that already provides linked list or queue-like functionality.
 }
