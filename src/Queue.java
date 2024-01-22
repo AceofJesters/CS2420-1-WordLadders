c1315fc115a79b6842bd4b0ad53c7017eea24af2
Basil Nelson <ems.nelson@hotmail.com>
2024-01-22 07:56:33 -0700

    sketch enque, deque, and variable setup

--- src/Queue.java
+++ src/Queue.java
@@ -1,32 +1,33 @@
-import java.io.File;
-import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Queue<E> {
+    private LadderNode<E> listHead = new LadderNode<>();
+    private LadderNode<E> listTail;
+    private int LadderSize;
 
     public void enqueue(E value){
 
     }
 
     public E dequeue(){
-
-        return null;
+        LadderNode<E> node = listHead.next;
+        if (listHead.next == null) {
+            listTail = listHead;
+        } else {
+            listHead.next = LadderNode.next;
+        }
+        LadderSize--;
+        return LadderNode.nodeValue;
     }
 
     public boolean isEmpty(){
-
-        return false;
+        return this.LadderSize == 0;
     }
 
     public class LadderNode<E> {
         public E nodeValue;
         public LadderNode<E> next;
-
         public LadderNode() { }
         public LadderNode(E nodeValue) {
             this.nodeValue = nodeValue;
         }
     }
-    //Use a linked list for the internal queue data representation.  You must write this linked list code yourself,
-    // may not use a Java collection that already provides linked list or queue-like functionality.
 }
